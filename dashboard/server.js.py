c159e20240d75fe050c365a33a03a8c42ae4d812
Shanreed25 <shannontlreed@gmail.com>
2025-09-01 20:47:51 -0700

    added display inline

--- dashboard/server.js.py
+++ dashboard/server.js.py
@@ -14,5 +14,9 @@ def background():
 def background_functions():
     return render_template("./background/backgroundfunctions.html")
 
+
+@app.route("/display")
+def display():
+    return render_template("./display/display.html")
 if __name__ == "__main__":
     app.run(debug=True)
