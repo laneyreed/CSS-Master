d28f222342b83c5021b93c1b017ea755389eb05b
Shanreed25 <shannontlreed@gmail.com>
2025-09-06 13:43:12 -0700

    adding templates

--- dashboard/server.js.py
+++ dashboard/server.js.py
@@ -6,17 +6,26 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/html")
+def html_home():
+    return render_template("html/html-home.html")
+
+@app.route("/css")
+def css_home():
+    return render_template("css/css-home.html")
+
+
 @app.route("/background")
 def background():
-    return render_template("./background/background.html")
+    return render_template("./css/background/background.html")
 
 @app.route("/background-functions")
 def background_functions():
-    return render_template("./background/backgroundfunctions.html")
+    return render_template("./css/background/backgroundfunctions.html")
 
 
 @app.route("/display")
 def display():
-    return render_template("./display/display.html")
+    return render_template("./css/display/display.html")
 if __name__ == "__main__":
     app.run(debug=True)
